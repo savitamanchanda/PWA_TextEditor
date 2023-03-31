eaa145ea59e61562c488c1f7b9f3541d6eea7fc0
Savita Rankavat <savita@Savitas-MacBook-Pro.local>
2023-03-31 15:16:23 -0500

    fixing error

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -32,11 +32,11 @@ export const putDb = async (content) => {
 export const getDb = async () => {
   console.log('GET from the database');
 
-  const jateDb = await openDB('content', 1);
+  const jateDb = await openDB('jate', 1);
 
-  const tx = jateDb.transaction('content', 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
 
-  const store = tx.objectStore('content');
+  const store = tx.objectStore('jate');
 
   const request = store.getAll();
 
